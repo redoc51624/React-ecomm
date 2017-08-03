a599d78a69b3e7e34f57161105404e104dbf6774
vikas kumar <vikas10145.12@bitmesra.ac.in>
2017-08-03 18:53:30 +0530

    modal remoed local

--- src/ProductDescrp.js
+++ src/ProductDescrp.js
@@ -1,42 +1,8 @@
 import React from 'react';
 import './ProductDescrp.css';
-import Modal from 'react-modal';
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
+
 
 class ProductDescrp extends React.Component {
-	constructor() {
-    super();
- 
-    this.state = {
-      modalIsOpen: false
-    };
- 
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
- 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
- 
-  afterOpenModal() {
-    // references are now sync'd and can be accessed. 
-    this.subtitle.style.color = '#f00';
-  }
- 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
   render() {
     return (
       <div className="container homePge contents col-xs-12 col-md-12 col-sm-12 col-lg-12">
